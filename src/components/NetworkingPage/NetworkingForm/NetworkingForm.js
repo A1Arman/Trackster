3c6b80bff6db67f4d7d2a95f67028b67ba542887
SuitcaseCoder <lauraruizroehrs@gmail.com>
2019-09-16 16:21:58 -0500

    Added Networking Redux Form

---
+++ src/components/NetworkingPage/NetworkingForm/NetworkingForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { reduxForm , Field } from 'redux-form';
+import renderFieldNetworking from '../renderFieldNetworking';
+
+class NetworkingForm extends React.Component {
+    onSubmit(values) {
+        console.log(values);
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
+                <label htmlFor="eventTitle">
+                <Field 
+                    name="eventTitle"
+                    id="eventTitle"
+                    type="text"
+                    component={renderFieldNetworking}
+                />
+                Event Title
+                </label>
+
+                <label htmlFor="eventLocation">
+                <Field 
+                    name="eventLocation" 
+                    id="eventLocation" 
+                    type="text" 
+                    component={renderFieldNetworking} 
+                />
+                Event Location
+                </label>
+
+                <label htmlFor="eventDate">
+                <Field 
+                    name="eventDate" 
+                    id="eventDate" 
+                    type="text" 
+                    component={renderFieldNetworking} 
+                />
+                Event Date
+                </label>
+
+                <label htmlFor="eventTime">
+                <Field 
+                    name="eventTime" 
+                    id="eventTime" 
+                    type="text" 
+                    component={renderFieldNetworking} 
+                />
+                Event Time
+                </label>
+
+                <button type="submit">Submit Event</button>
+
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'networking'
+})(NetworkingForm);
